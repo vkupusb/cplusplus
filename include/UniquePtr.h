75ae2ea2655c85c423715286893f8cf7f2808d4c
vkupusb <kvishva27@gmail.com>
2025-04-06 19:04:27 +0530

    updated logic

--- include/UniquePtr.h
+++ include/UniquePtr.h
@@ -21,16 +21,86 @@ namespace Pointers::SmartPointers::Unique
         UniquePtr(UniquePtr &&moveObject);
         UniquePtr &operator=(UniquePtr &&moveObject);
 
-        T& operator*() const;
-        T* operator->() const;
-        T* get() const;
-        T* release();
-        void reset(T* ptr=nullptr);
+        T &operator*() const;
+        T *operator->() const;
+        T *get() const;
+        T *release();
+        void reset(T *ptr = nullptr);
         explicit operator bool() const;
 
     private:
         T *m_ptr;
     };
 
- 
+    template <class T>
+    UniquePtr<T>::UniquePtr(T *ptr) : m_ptr{ptr}
+    {
+        std::cout << "UniquePtr constructor called...\n";
+    }
+
+    template <class T>
+    UniquePtr<T>::~UniquePtr()
+    {
+        std::cout << "UniquePtr destructor called...\n";
+        delete m_ptr;
+    }
+
+    template <class T>
+    UniquePtr<T>::UniquePtr(UniquePtr &&moveObject) : m_ptr{moveObject.m_ptr}
+    {
+        std::cout << "UniquePtr move constructor called...\n";
+        moveObject.m_ptr = nullptr; // avoid deleteing twice
+    }
+
+    template <class T>
+    UniquePtr<T> &UniquePtr<T>::operator=(UniquePtr &&moveObject)
+    {
+        if (this != &moveObject)
+        {
+            delete m_ptr;
+            m_ptr = moveObject.m_ptr;
+            moveObject.m_ptr = nullptr;
+        }
+        return *this;
+    }
+
+    template <class T>
+    T &UniquePtr<T>::operator*() const
+    {
+        return *m_ptr;
+    }
+
+    template <class T>
+    T *UniquePtr<T>::operator->() const
+    {
+        return m_ptr;
+    }
+
+    template <class T>
+    T *UniquePtr<T>::get() const
+    {
+        return m_ptr;
+    }
+
+    template <class T>
+    T *UniquePtr<T>::release()
+    {
+        T *temp = m_ptr;
+        m_ptr = nullptr;
+        return temp;
+    }
+
+    template <class T>
+    void UniquePtr<T>::reset(T *ptr)
+    {
+        delete m_ptr;
+        m_ptr = ptr;
+    }
+
+    template <class T>
+    UniquePtr<T>::operator bool() const
+    {
+        return m_ptr != nullptr;
+    }
+
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include "UniquePtr.h"
 
+class A{
+    public:
+    A(){std::cout<<"A constructor called ...\n";}
+    virtual ~A(){std::cout<<"A destructor called ...\n";}
+    virtual void display(){std::cout<<" A::display()...\n";}
+    
+};
+
+class B: virtual public A{
+    public:
+    B(){std::cout<<"B constructor called ...\n";}
+    ~B(){std::cout<<"B destructor called ...\n";}
+    void display(){std::cout<<" B::display()...\n";}
+};
+
+class C: virtual public A{
+    public:
+    C(){std::cout<<"C constructor called ...\n";}
+    ~C(){std::cout<<"C destructor called ...\n";}
+    void display(){std::cout<<" C::display()...\n";}
+
+};
+
+class D: public B, public C{
+    public:
+    D(){std::cout<<"D constructor called ...\n";}
+    ~D(){std::cout<<"D destructor called ...\n";}
+    void display(){std::cout<<" D::display()...\n";}
+};
+
 int main(int agrc,char** argv)
 {
-    Pointers::SmartPointers::Unique::UniquePtr<int> a{nullptr};
+    Pointers::SmartPointers::Unique::UniquePtr<A> a{new D};
+    a->display();
 
     return 0;
 }
